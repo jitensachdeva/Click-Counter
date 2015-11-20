932be90923f9e6dc627444dcf6dd80e046eb6825
Jitendra Sachdeva <jitensachdeva@gmail.com>
2015-11-20 19:00:07 +0530

    added click button and label

--- ViewController.swift
+++ ViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        let button = UIButton()
+        button.frame = CGRectMake(150,250, 60,60)
+        button.setTitle("click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: "incrementCount", forControlEvents: .TouchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func incrementCount(){
+        count++
+        label.text = "\(count)"
     }
 
 
